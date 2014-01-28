04ab71eb06796e0d32b7442a7c4682a8cdb1cbd8
Michiel Scholten <michiel@aquariusoft.org>
2014-01-28 08:31:47 +0100

    Added logging, initial memcache (at the moment only to check whether we should run)

--- notifications_pushbullet.py
+++ notifications_pushbullet.py
@@ -1,7 +1,10 @@
-import ns-api.ns_api
+import sys
 import datetime
+import logging
+import ns-api.ns_api
 import settings
 from pushbullet import PushBullet
+import pylibmc
 
 def unique(my_list): 
     result = []
@@ -14,10 +17,43 @@ def unique(my_list):
 MAX_TIME_PAST = 1800
 MAX_TIME_FUTURE = 3600
 
+# create logger
+logger = logging.getLogger('ns_api')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('ns_api.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
 api_key = settings.pushbullet_key
-p = PushBullet(apiKey)
-# Get a list of devices
+p = PushBullet(api_key)
+# Get a list of devices. A device to push this to will be selected later on
 devices = p.getDevices()
+
+logger.debug(devices)
+
+mc = pylibmc.Client(['127.0.0.1'], binary=True, behaviors={'tcp_nodelay': True, 'ketama': True})
+
+should_run = True
+if 'run' in mc:
+    should_run = mc['run']
+else:
+    logger.info('no run tuple in memcache, creating')
+    mc['run'] = should_run
+
+logger.debug('should_run: %s' % should_run)
+
+if should_run == False:
+    sys.exit(0)
 
 delays = []
 
@@ -30,13 +66,16 @@ for route in settings.routes:
     delta = current_time - route_time
     if current_time > route_time and delta.total_seconds() > MAX_TIME_PAST:
         # the route was too long ago ago, lets skip it
+        logger.info('route %s was too long ago, skipped' % route)
         continue
     if current_time < route_time and abs(delta.total_seconds()) > MAX_TIME_FUTURE:
         # the route is too much in the future, lets skip it
+        logger.info('route %s was too much in the future, skipped' % route)
         continue
 
     route_delays, vertrekken = ns_api.vertrektijden(route['departure'])
     for vertrek in vertrekken:
+        logger.debug(vertrek)
         #print vertrek
         if route['keyword'] == None:
             if vertrek['delay'] > 0  and route['destination'] in vertrek['route']:
@@ -54,15 +93,21 @@ for route in settings.routes:
     route_text = 'Route {0} - {1} van {2}'.format(route['departure'], route['destination'], route['time'])
 
     planned_route = ns_api.route(route['departure'], route['destination'], '', today, route['time'])
+    logger.debug(planned_route)
     if planned_route[0]['departure_delay'] > 0:
         delays.append("{0}\nVertrekvertraging: {1} minuten op {2}".format(route_text, planned_route[0]['departure_delay'], planned_route[0]['departure_platform']))
     if planned_route[0]['arrival_delay'] > 0:
         delays.append("{0}\nAankomstvertraging: {1} minuten op {2}".format(route_text, planned_route[0]['arrival_delay'], planned_route[0]['arrival_platform']))
 
+logger.debug(delays)
+
 # deduplicate the list (useful when having multiple routes from the same origin):
 if len(delays) > 1:
     delays = unique(delays)
 
+logger.debug(delays)
+
 if len(delays) > 0:
     # Send a note with all delays to device 5 of the list from PushBullet:
-    p.pushNote(devices[5]["id"], 'NS Vertraging', "\n\n".join(delays))
+    logger.info('sending delays to device %s' % settings.device_index)
+    p.pushNote(devices[settings.device_index]["id"], 'NS Vertraging', "\n\n".join(delays))
