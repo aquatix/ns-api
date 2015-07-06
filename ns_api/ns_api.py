d81c79e6d665800d2a42fb1c857b506a8ab60872
Michiel Scholten <michiel@diginaut.net>
2015-07-06 15:53:35 +0200

    Fix all the things

--- ns_api/ns_api.py
+++ ns_api/ns_api.py
@@ -2,13 +2,16 @@
 Library to query the official Dutch railways API
 """
 import urllib2
+import urllib
 import xmltodict
 import time
 
 from datetime import datetime, timedelta
 
-from pytz import timezone, utc
 from pytz.tzinfo import StaticTzInfo
+
+import json
+from collections import OrderedDict
 
 
 class OffsetTime(StaticTzInfo):
@@ -20,109 +23,118 @@ class OffsetTime(StaticTzInfo):
         minutes = int(offset[0] + offset[3:])
         self._utcoffset = timedelta(hours=hours, minutes=minutes)
 
-def load_datetime(value, format):
-    if format.endswith('%z'):
-        format = format[:-2]
+def load_datetime(value, dt_format):
+    """
+    Create timezone-aware datetime object
+    """
+    if dt_format.endswith('%z'):
+        dt_format = dt_format[:-2]
         offset = value[-5:]
         value = value[:-5]
-        return OffsetTime(offset).localize(datetime.strptime(value, format))
+        return OffsetTime(offset).localize(datetime.strptime(value, dt_format))
 
-    return datetime.strptime(value, format)
+    return datetime.strptime(value, dt_format)
 
-def dump_datetime(value, format):
-    return value.strftime(format)
+def dump_datetime(value, dt_format):
+    """
+    Format datetime object to string
+    """
+    return value.strftime(dt_format)
 
 
-class Departure():
+class Departure(object):
+    """
+    Information on a departing train on a certain station
+    """
 
     def __init__(self, departure_dict):
-        trip_number = departure_dict['RitNummer']
-        departure_time = departure_dict['VertrekTijd']
+        self.trip_number = departure_dict['RitNummer']
+        self.departure_time = departure_dict['VertrekTijd']
         try:
-            has_delay = True
-            departure_delay = departure_dict['VertrekVertraging']
-            departure_delay_text = departure_dict['VertrekVertragingTekst']
-        except:
-            has_delay = False
-        departure_platform = departure_dict['VertrekSpoor']
-        departure_platform_changed = departure_dict['VertrekSpoor']['@wijziging']
-
-        destination = departure_dict['EindBestemming']
-        try:
-            route_text = departure_dict['RouteTekst']
+            self.has_delay = True
+            self.departure_delay = departure_dict['VertrekVertraging']
+            self.departure_delay_text = departure_dict['VertrekVertragingTekst']
         except KeyError:
-            route_text = None
+            self.has_delay = False
+        self.departure_platform = departure_dict['VertrekSpoor']
+        self.departure_platform_changed = departure_dict['VertrekSpoor']['@wijziging']
 
-        train_type = departure_dict=['TreinSoort']
-        carrier = departure_dict=['Vervoerder']
+        self.destination = departure_dict['EindBestemming']
+        try:
+            self.route_text = departure_dict['RouteTekst']
+        except KeyError:
+            self.route_text = None
+
+        self.train_type = departure_dict = ['TreinSoort']
+        self.carrier = departure_dict = ['Vervoerder']
 
         try:
-            journey_tip = departure_dict=['ReisTip']
+            self.journey_tip = departure_dict = ['ReisTip']
         except KeyError:
-            journey_tip = None
+            self.journey_tip = None
 
         try:
-            remarks = departure_dict=['Opmerkingen']
+            self.remarks = departure_dict = ['Opmerkingen']
         except KeyError:
-            remarks = []
+            self.remarks = []
 
 
     def __unicode__(self):
         return 'trip_number: ' + self.trip_number
 
 
-class TripStop():
+class TripStop(object):
 
     def __init__(self, part_dict):
-        name = part_dict['Naam']
-        time = part_dict['Tijd']
-        platform = part_dict['Spoor']
+        self.name = part_dict['Naam']
+        self.time = part_dict['Tijd']
+        self.platform = part_dict['Spoor']
 
 
-class TripSubpart():
+class TripSubpart(object):
 
     def __init__(self, part_dict):
-        trip_type = part_dict['@reisSoort']
-        transporter = part_dict['Vervoerder']
-        transport_type = part_dict['VervoerType']
-        journey_id = part_dict['RitNummer']
-        status = part_dict['Status']
+        self.trip_type = part_dict['@reisSoort']
+        self.transporter = part_dict['Vervoerder']
+        self.transport_type = part_dict['VervoerType']
+        self.journey_id = part_dict['RitNummer']
+        self.status = part_dict['Status']
 
     def __unicode__(self):
         print self.trip_type
         return self.trip_type
 
 
-class Trip():
+class Trip(object):
 
     def __init__(self, trip_dict):
-        nr_transfers = trip_dict['AantalOverstappen']
-        travel_time_planned = trip_dict['GeplandeReisTijd']
-        travel_time_actual = trip_dict['ActueleReisTijd']
-        is_optimal = True if trip_dict['Optimaal'] == 'true' else False
-        status = trip_dict['Status']
+        self.nr_transfers = trip_dict['AantalOverstappen']
+        self.travel_time_planned = trip_dict['GeplandeReisTijd']
+        self.travel_time_actual = trip_dict['ActueleReisTijd']
+        self.is_optimal = True if trip_dict['Optimaal'] == 'true' else False
+        self.status = trip_dict['Status']
 
-        format = "%Y-%m-%dT%H:%M:%S%z"
-
-        try:
-            departure_time_planned = load_datetime(trip_dict['GeplandeVertrekTijd'], format)
-        except:
-            departure_time_planned = None
+        dt_format = "%Y-%m-%dT%H:%M:%S%z"
 
         try:
-            departure_time_actual = load_datetime(trip_dict['ActueleVertrekTijd'], format)
+            self.departure_time_planned = load_datetime(trip_dict['GeplandeVertrekTijd'], dt_format)
         except:
-            departure_time_actual = None
+            self.departure_time_planned = None
 
         try:
-            arrival_time_planned = load_datetime(trip_dict['GeplandeAankomstTijd'], format)
+            self.departure_time_actual = load_datetime(trip_dict['ActueleVertrekTijd'], dt_format)
         except:
-            arrival_time_planned = None
+            self.departure_time_actual = None
 
         try:
-            arrival_time_actual = load_datetime(trip_dict['ActueleAankomstTijd'], format)
+            self.arrival_time_planned = load_datetime(trip_dict['GeplandeAankomstTijd'], dt_format)
         except:
-            arrival_time_actual = None
+            self.arrival_time_planned = None
+
+        try:
+            self.arrival_time_actual = load_datetime(trip_dict['ActueleAankomstTijd'], dt_format)
+        except:
+            self.arrival_time_actual = None
 
 
         trip_parts = trip_dict['ReisDeel']
@@ -132,7 +144,7 @@ class Trip():
         print(len(trip_parts))
         print '-------'
 
-        trip_parts = []
+        self.trip_parts = []
         for part in trip_dict['ReisDeel']:
             print('-- part --')
             print(part)
@@ -143,26 +155,41 @@ class Trip():
                 print part
                 print('-- /changed part --')
             trip_part = TripSubpart(part)
-            trip_parts.append(trip_part)
+            self.trip_parts.append(trip_part)
 
-        print trip_parts
+        print self.trip_parts
 
     def to_json(self):
         """
         Create a JSON representation of this model
         """
         # TODO implement
-        pass
+        return json.dumps(OrderedDict(self.__dict__))
 
-    def from_json(self):
+    def from_json(self, source_json):
         """
         Parse a JSON representation of this model back to, well, the model
         """
         # TODO implement
+        # json.JSONDecoder(object_pairs_hook=collections.OrderedDict).decode(source_json)
         pass
 
+    def delay_text(self):
+        """
+        If trip has delays, format a natural language summary
+        """
+        # TODO implement
+        pass
+
+    def __repr__(self):
+        #return 'Trip repr'
+        return self.__unicode__()
+
+    def __str__(self):
+        return self.__unicode__()
+
     def __unicode__(self):
-        return 'departure_time_planned: ' + self.departure_time_planned
+        return '<Trip> plan: {0} actual: {1} transfers: {2}'.format(self.departure_time_planned, self.departure_time_actual, self.nr_transfers)
 
 
 def parse_departures(xml):
@@ -176,7 +203,11 @@ def parse_departures(xml):
     for departure in obj['ActueleVertrekTijden']['VertrekkendeTrein']:
         newdep = Departure(departure)
         departures.append(newdep)
+        print('-- departure --')
+        print(newdep)
         print repr(newdep)
+        print(newdep.__dict__)
+        print('-- /departure --')
 
     return departures
 
@@ -188,7 +219,7 @@ def get_departures(station):
     """
     url = 'http://www.ns.nl/actuele-vertrektijden/main.action?xml=true'
     user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-    header = { 'User-Agent' : user_agent }
+    header = {'User-Agent' : user_agent}
 
     values = {
         'van_heen_station' : station,
@@ -198,7 +229,7 @@ def get_departures(station):
     req = urllib2.Request(url, data, header)
     response = urllib2.urlopen(req)
     page = response.read()
-    soup = BeautifulSoup(page)
+    #soup = BeautifulSoup(page)
     disruptions = []
 
 
@@ -213,7 +244,11 @@ def parse_trips(xml):
         #print departure
         newtrip = Trip(trip)
         trips.append(newtrip)
+        print('-- trip --')
         print repr(newtrip)
+        print(newtrip)
+        print(newtrip.__dict__)
+        print('-- /trip --')
 
 
 def get_trips(starttime, start, via, destination):
