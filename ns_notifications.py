cebacfb866d464f9557300f79e2599d1eecfad76
Michiel Scholten <michiel@diginaut.net>
2015-07-06 08:03:17 +0200

    Cleanups

--- ns_notifications.py
+++ ns_notifications.py
@@ -2,13 +2,10 @@
 NS trip notifier
 """
 from ns_api import ns_api
-import xmltodict
 from pushbullet import PushBullet
 import pylibmc
-import urllib2
 #import simplejson as json
 import __main__ as main
-
 
 
 mc = pylibmc.Client(['127.0.0.1'], binary=True, behaviors={'tcp_nodelay': True, 'ketama': True})
@@ -27,28 +24,10 @@ if __name__ == '__main__':
     """
     print("Run!")
 
-    #with open('actuele_vertrektijden.xml') as fd:
-    #with open('examples.xml') as fd:
-    #    obj = xmltodict.parse(fd.read())
     departures = []
     with open('examples.xml') as fd:
         departures = ns_api.parse_departures(fd.read())
 
-
-    #for departure in obj['ActueleVertrekTijden']:
-    #    print departure['VertrekkendeTrein']
-    #for departure in obj['ActueleVertrekTijden']['VertrekkendeTrein']:
-    #    #print departure
-    #    newdep = ns_api.Departure(departure)
-    #    departures.append(newdep)
-    #    print repr(newdep)
-    #    #print(json.dumps(newdep))
-
-
-
-    #with open('reismogelijkheden.xml') as fd:
-    #    obj = xmltodict.parse(fd.read())
     trips = []
     with open('reismogelijkheden.xml') as fd:
         trips = ns_api.parse_trips(fd.read())
-
