ed8164a5dc5122a9824e3a6915025795db7744e8
Michiel Scholten <michiel@diginaut.net>
2015-07-20 12:25:05 +0200

    Deserialisation robustness change

--- ns_api/ns_api.py
+++ ns_api/ns_api.py
@@ -64,30 +64,35 @@ def list_from_json(source_list_json):
     Deserialise all the items in source_list from json
     """
     result = []
-    print source_list_json
     if source_list_json == []:
         return result
     for list_item in source_list_json:
         item = json.loads(list_item)
-        if item['class_name'] == 'Trip':
-            temp = Trip()
-        elif item['class_name'] == 'Departure':
-            temp = Departure()
-        elif item['class_name'] == 'Disruption':
-            temp = Disruption()
-        elif item['class_name'] == 'Station':
-            temp = Station()
-        elif item['class_name'] == 'Trip':
-            temp = Trip()
-        elif item['class_name'] == 'TripRemark':
-            temp = TripRemark()
-        elif item['class_name'] == 'TripStop':
-            temp = TripStop()
-        elif item['class_name'] == 'TripSubpart':
-            temp = TripSubpart()
-        temp.from_json(list_item)
-        result.append(temp)
-    print result
+        try:
+            if item['class_name'] == 'Trip':
+                temp = Trip()
+            elif item['class_name'] == 'Departure':
+                temp = Departure()
+            elif item['class_name'] == 'Disruption':
+                temp = Disruption()
+            elif item['class_name'] == 'Station':
+                temp = Station()
+            elif item['class_name'] == 'Trip':
+                temp = Trip()
+            elif item['class_name'] == 'TripRemark':
+                temp = TripRemark()
+            elif item['class_name'] == 'TripStop':
+                temp = TripStop()
+            elif item['class_name'] == 'TripSubpart':
+                temp = TripSubpart()
+            else:
+                print('Unrecognised Class ' + item['class_name'] + ', skipping')
+                continue
+            temp.from_json(list_item)
+            result.append(temp)
+        except KeyError:
+            print('Unrecognised item with no class_name, skipping')
+            continue
     return result
 
 
